f761e967a22e7a33bf2f125778175503889b9983
Stu W <stubillwhite@gmail.com>
2020-12-02 18:19:21 +0000

    Solution for day two part two

--- src/AdventOfCode/Day02.hs
+++ src/AdventOfCode/Day02.hs
@@ -1,5 +1,5 @@
 module AdventOfCode.Day02
-  ( day02, solutionOne
+  ( day02, solutionOne, solutionTwo
   ) where 
 
 import Text.ParserCombinators.Parsec
@@ -26,12 +26,12 @@ parser = many1 $ passwordEntry <* newline
 parseInput :: String -> Either ParseError [PasswordEntry]
 parseInput input = parse parser "Failed to parse" input
 
--- >>> isValidPassword (PasswordEntry 1 2 'c' "abc")
+-- >>> isValidSledRentalPassword (PasswordEntry 1 2 'c' "abc")
 -- True
--- >>> isValidPassword (PasswordEntry 2 3 'c' "abc")
+-- >>> isValidSledRentalPassword (PasswordEntry 2 3 'c' "abc")
 -- False
-isValidPassword :: PasswordEntry -> Bool
-isValidPassword PasswordEntry{a,b,ch,password} = 
+isValidSledRentalPassword :: PasswordEntry -> Bool
+isValidSledRentalPassword PasswordEntry{a,b,ch,password} = 
     a <= count && count <= b
   where
     count = (length . filter (== ch)) password
@@ -40,11 +40,29 @@ solutionOne :: String -> Either ParseError Int
 solutionOne input = 
     validPasswords
   where
-    validPasswords = fmap (length . filter isValidPassword) (parseInput input)
+    validPasswords = fmap (length . filter isValidSledRentalPassword) (parseInput input)
 
+-- Part two
+
+-- >>> isValidTobogganRentalPassword (PasswordEntry 1 2 'c' "abc")
+-- False
+-- >>> isValidTobogganRentalPassword (PasswordEntry 2 3 'c' "abc")
+-- True
+isValidTobogganRentalPassword :: PasswordEntry -> Bool
+isValidTobogganRentalPassword PasswordEntry{a,b,ch,password} = 
+    (length . filter id) [charMatchesAt a, charMatchesAt b] == 1
+  where
+    charMatchesAt = \x -> ch == password !! (x - 1)
+
+solutionTwo :: String -> Either ParseError Int
+solutionTwo input = 
+    validPasswords
+  where
+    validPasswords = fmap (length . filter isValidTobogganRentalPassword) (parseInput input)
 
 day02 :: IO ()
 day02 = do  
     problemInput <- readFile "input/day-02.txt"
     putStrLn "Day 2"
     putStrLn $ "  Part one: " ++ show (solutionOne problemInput)
+    putStrLn $ "  Part two: " ++ show (solutionTwo problemInput)
--- test/AdventOfCode/Day02Spec.hs
+++ test/AdventOfCode/Day02Spec.hs
@@ -2,7 +2,7 @@ module AdventOfCode.Day02Spec (main, spec) where
 
 import Test.Hspec ( hspec, describe, it, shouldBe, Spec )
 
-import AdventOfCode.Day02 ( day02, solutionOne )
+import AdventOfCode.Day02 ( day02, solutionOne, solutionTwo )
 
 main :: IO ()
 main = hspec spec
@@ -12,5 +12,8 @@ spec = do
   describe "solutionOne" $ do
     it "should return expected responses for example data" $ do
       solutionOne exampleData `shouldBe` Right 2
+  describe "solutionTwo" $ do
+    it "should return expected responses for example data" $ do
+      solutionTwo exampleData `shouldBe` Right 1
   where
     exampleData = unlines ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
